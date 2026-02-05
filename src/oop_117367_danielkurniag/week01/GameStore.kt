37183cc6248d183eed1eef6e88039bbfd018d49b
EL-43 <danielkurniagunawan@gmail.com>
2026-02-05 15:50:51 +0700

    week01: (tugas 3) implement receipt printing & fixed discount logic

--- src/oop_117367_danielkurniag/week01/GameStore.kt
+++ src/oop_117367_danielkurniag/week01/GameStore.kt
@@ -1,9 +1,22 @@
 package oop_117367_danielkurniag.week01
 
-fun calculateDiscount(price: Int) = if (price > 500000) "20% discount" else "10% discount"
+fun calculateDiscount(price: Int) : Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
+
+fun printReceipt(title : String, finalPrice: Int) = println("Title: $title\nPrice: $finalPrice")
 
 fun main(){
     //declarations
     val gameTitle = "Library Of Ruina"
     val price = 50000
+
+    val finalPrice = price - calculateDiscount(price)
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
+
+
+//println(calculateDiscount(price))
+
+//println("Price: $finalPrice")
+//  println(printReceipt(gameTitle, $finalPrice))
+
 }
