256e6106981f2f47f8c6bb719448bec68ab28e6b
EL-43 <danielkurniagunawan@gmail.com>
2026-02-05 15:14:53 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_117367_danielkurniag/week01/GameStore.kt
+++ src/oop_117367_danielkurniag/week01/GameStore.kt
@@ -1,5 +1,7 @@
 package oop_117367_danielkurniag.week01
 
+fun calculateDiscount(price: Int) = if (price > 500000) "20% discount" else "10% discount"
+
 fun main(){
     //declarations
     val gameTitle = "Library Of Ruina"
