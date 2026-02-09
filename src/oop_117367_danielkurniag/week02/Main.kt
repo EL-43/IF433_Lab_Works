1ab0e1aa4c8acf33316f4820023a96e320917cc7
EL-43 <danielkurniagunawan@gmail.com>
2026-02-09 15:24:45 +0700

    week02: implement selection logic for constructors

--- src/oop_117367_danielkurniag/week02/Main.kt
+++ src/oop_117367_danielkurniag/week02/Main.kt
@@ -2,7 +2,7 @@ package oop_117367_danielkurniag.week02
 
 import java.util.Scanner
 
-fun main(){
+fun main() {
     val scanner = Scanner(System.`in`)
 
     println("--- UMN PMB ---")
@@ -15,14 +15,24 @@ fun main(){
 
     scanner.nextLine()
 
-    if (nim.length != 5){
+    if (nim.length != 5) {
         println("ERROR: Enrollment cancelled, NIM must be 5 characters!")
-    } else {
+    }
+
+    print("Choose route (1. Regular, 2. General): ")
+    val type = scanner.nextInt()
+    scanner.nextLine()
+
+    if (type == 1) {
         print("Enter Major: ")
         val major = scanner.nextLine()
-
         val s1 = Student(name, nim, major)
-        println("Status: Enrollment finished.")
+        println("Enrolled in ${s1.major} with starting GPA ${s1.gpa}")
+    } else if (type == 2) {
+        val s2 = Student(name, nim)
+        println("Enrolled in ${s2.major} with starting GPA ${s2.gpa}")
+    } else {
+        println("Invalid choice! enrollment cancelled.")
     }
 
 
