f165b8da660fe7b5c192e179186d7094fa273c0f
EL-43 <danielkurniagunawan@gmail.com>
2026-02-09 15:15:23 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_117367_danielkurniag/week02/Student.kt
+++ src/oop_117367_danielkurniag/week02/Student.kt
@@ -1,6 +1,6 @@
 package oop_117367_danielkurniag.week02
 
-class Student(val name: String, val nim: String, var major: String) {
+class Student(val name: String, val nim: String, var major: String, val ipk: Double = 0.0) {
     constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated"){
         println("LOG: MENGGUNAKAN CONSTRUCTOR JALUR UMUM (TANPA JURUSAN)")
     }
