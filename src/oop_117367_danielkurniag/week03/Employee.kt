f57987b0ca08d3d120c9275236d83e6ce7a5eff1
EL-43 <danielkurniagunawan@gmail.com>
2026-02-16 15:08:06 +0700

    week03: fix setter using backing field

--- src/oop_117367_danielkurniag/week03/Employee.kt
+++ src/oop_117367_danielkurniag/week03/Employee.kt
@@ -3,8 +3,11 @@ package oop_117367_danielkurniag.week03
 class Employee(val name: String) {
     var salary: Int = 0
         set(value)  {
-            println("Mencoba set gaji ke $value")
-            //WARNING, THE CODE BELOW IS DANGEROUS
-            this.salary = value
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0
+            } else {
+                field = value
+            }
         }
 }
--- src/oop_117367_danielkurniag/week03/Main.kt
+++ src/oop_117367_danielkurniag/week03/Main.kt
@@ -3,5 +3,5 @@ package oop_117367_danielkurniag.week03
 fun main(){
     val e = Employee("Budi")
     println("--- test error ---")
-    e.salary = 5000 //this will blow up
+    e.salary = 5000 //this will blow up the program
 }
