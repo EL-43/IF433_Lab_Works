8cd37d7f024d12f87276b2a1ce4518e023d14cc3
EL-43 <danielkurniagunawan@gmail.com>
2026-02-23 15:17:11 +0700

    week04:(task 1) test ElectricCar in ElectricCarTest.kt

---
+++ src/oop_117367_danielkurniag/week04/ElectricCarTest.kt
@@ -0,0 +1,8 @@
+package oop_117367_danielkurniag.week04
+
+fun main(){
+    val myElectricCar = ElectricCar("Tesla EV", 4, 90)
+    myElectricCar.accelerate()
+    myElectricCar.honk()
+    myElectricCar.openTrunk()
+}
