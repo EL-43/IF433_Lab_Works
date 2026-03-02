2f656253fc7de1f91ba11ed3eede6483265abc68
EL-43 <danielkurniagunawan@gmail.com>
2026-03-02 15:45:48 +0700

    week05: (task 1) test MathHelper overloading in Math.kt

---
+++ src/oop_117367_danielkurniag/week05/Math.kt
@@ -0,0 +1,8 @@
+package oop_117367_danielkurniag.week05
+
+fun main(){
+    val math = MathHelper()
+    println(math.hitungLuas(50))
+    println(math.hitungLuas(20, 30))
+    println(math.hitungLuas(52.0))
+}
