71a293ceafe6f72e89a250b0f0cb657f46595c5a
EL-43 <danielkurniagunawan@gmail.com>
2026-03-09 16:22:11 +0700

    week06: (task) test smart home system logic in SmartHomeTesting.kt

--- src/oop_117367_danielkurniag/week06/SmartHomeTesting.kt
+++ src/oop_117367_danielkurniag/week06/SmartHomeTesting.kt
@@ -4,4 +4,10 @@ fun main(){
     val ruangTamu = SmartLamp()
     val googleNestDapur = SmartSpeaker()
     val evizGarasi = SmartCCTV()
+    val hub = SmartHomeHub()
+    hub.addDevice(ruangTamu)
+    hub.addDevice(googleNestDapur)
+    hub.addDevice(evizGarasi)
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }
