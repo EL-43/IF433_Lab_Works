d6dcb0cf76b87d22a3df49b1bd4e0ce60cd515d7
EL-43 <danielkurniagunawan@gmail.com>
2026-03-09 16:16:49 +0700

    week06: (task) create SmartCCTV with compound interfaces

--- src/oop_117367_danielkurniag/week06/SmartCCTV.kt
+++ src/oop_117367_danielkurniag/week06/SmartCCTV.kt
@@ -1,18 +1,16 @@
 package oop_117367_danielkurniag.week06
 
 class SmartCCTV : SmartDevice, Switchable, Recordable {
-    override val id: String = "01"
-    override val name: String = "SmartLamp"
+    override val id: String = "03"
+    override val name: String = "SmartCCTV"
+    override fun startRecord(){
+        println("$name with id $id starting recording")
+    }
     override fun turnOn() {
         println("$name with id $id turned on")
-        fun startRecord() {
-            println("$name with id $id starting recording")
-        }
+        startRecord()
     }
     override fun turnOff() {
         println("$name with id $id turned off")
-    }
-    override fun startRecord() {
-
     }
 }
