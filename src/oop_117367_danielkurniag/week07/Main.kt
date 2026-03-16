35d8a2e687bbdc783596573c10dfac2aaf7a8b98
EL-43 <danielkurniagunawan@gmail.com>
2026-03-16 15:11:16 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/oop_117367_danielkurniag/week07/Main.kt
+++ src/oop_117367_danielkurniag/week07/Main.kt
@@ -5,7 +5,13 @@ fun main() {
     println("Status: ${DatabaseManager.connectionStatus}")
     DatabaseManager.connect()
 
-    println("\n=== COMPANION OBJECT TEST===")
+    println("\n=== COMPANION OBJECT TEST ===")
     val client = NetworkClient.createClient()
     client.connect()
+
+    println("\n=== REGULAR CLASS TEST ===")
+    val reg1 = RegularUser("Alice", 22)
+    val reg2 = RegularUser("Alice", 22)
+    println(reg1)
+    println("Are they the same? ${reg1 == reg2}")
 }
