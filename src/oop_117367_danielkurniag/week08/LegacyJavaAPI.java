fefe6825cbff48f1add54d49efff5adbb141cf7c
EL-43 <danielkurniagunawan@gmail.com>
2026-04-13 15:20:51 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_117367_danielkurniag/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_117367_danielkurniag.week08;
+
+public class LegacyJavaAPI {
+    //this function is written in Java, Kotlin doesn't know whether the return is Nullable.
+    public static String fetchServerStatus(){
+        return "SERVER_ONLINE_200_OK";
+    }
+}
