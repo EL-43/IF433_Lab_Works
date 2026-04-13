635fc5128b980037bd9e8656ef4fc217310c09dc
EL-43 <danielkurniagunawan@gmail.com>
2026-04-13 14:46:01 +0700

    week08: create nested classes for Order

---
+++ src/oop_117367_danielkurniag/week08/OrderModels.kt
@@ -0,0 +1,7 @@
+package oop_117367_danielkurniag.week08
+
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
+
