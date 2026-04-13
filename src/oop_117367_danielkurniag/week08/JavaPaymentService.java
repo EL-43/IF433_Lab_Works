06b42f6e79f08904cd61b4e7630caacdf44cc826
EL-43 <danielkurniagunawan@gmail.com>
2026-04-13 16:18:59 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_117367_danielkurniag/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_117367_danielkurniag.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productID){
+        return "TRX-" + productID + "-SUCCESS";
+    }
+}
