77bca214a103f14faaf3ac4c3146ae2c4217fb30
EL-43 <danielkurniagunawan@gmail.com>
2026-04-20 14:26:10 +0700

    week09: (task) chain sortedBy and map for the worst performers

--- src/oop_117367_danielkurniag/week09/TradingDashboard.kt
+++ src/oop_117367_danielkurniag/week09/TradingDashboard.kt
@@ -23,5 +23,9 @@ fun main() {
         .sortedByDescending { it.roe }
         .map { "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)" }
 
+    val worstPerformerString = losingTrades
+        .sortedBy { it.roe }
+        .map {"LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE(Lev: ${it.leverage}x)" }
+
 
 }
