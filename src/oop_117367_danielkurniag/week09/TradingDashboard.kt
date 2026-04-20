0f26de976186cbeb4f9ba87e0256a0f0491f3017
EL-43 <danielkurniagunawan@gmail.com>
2026-04-20 14:26:40 +0700

    week09: (task) extract unique trading pairs and toSet

--- src/oop_117367_danielkurniag/week09/TradingDashboard.kt
+++ src/oop_117367_danielkurniag/week09/TradingDashboard.kt
@@ -27,5 +27,8 @@ fun main() {
         .sortedBy { it.roe }
         .map {"LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE(Lev: ${it.leverage}x)" }
 
+    val uniquePairs = tradeHistory
+        .map { it.pair }.toSet()
+
 
 }
