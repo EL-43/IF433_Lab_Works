570a4f808d0c80a238e77e79c6c9ec655f0e1661
EL-43 <danielkurniagunawan@gmail.com>
2026-04-20 13:29:46 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_117367_danielkurniag/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_117367_danielkurniag.week09
+
+fun main(){
+    val tradeHistory = listOf(
+        TradeLog("Pair1", "Pos1", 20, 25.0, "OPEN"),
+        TradeLog("Pair2", "Pos2", 30, -30.0, "OPEN"),
+        TradeLog("Pair3", "Pos3", 40, -35.0, "CLOSED"),
+        TradeLog("Pair4", "Pos4", 50, -40.0, "OPEN"),
+        TradeLog("Pair5", "Pos5", 60, 45.0, "CLOSED"),
+        TradeLog("Pair6", "Pos6", 70, 50.0, "CLOSED"),
+    )
+}
