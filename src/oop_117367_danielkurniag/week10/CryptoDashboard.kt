2bea698e78726644e8bd9585495c5976d77557a4
EL-43 <danielkurniagunawan@gmail.com>
2026-04-27 15:30:14 +0700

    week10: (task) populate coin repository

--- src/oop_117367_danielkurniag/week10/CryptoDashboard.kt
+++ src/oop_117367_danielkurniag/week10/CryptoDashboard.kt
@@ -3,5 +3,8 @@ package oop_117367_danielkurniag.week10
 fun main(){
     val coinRepo = WalletRepository<Coin>()
 
+    coinRepo.add(Coin("BTC", 67000.0))
+    coinRepo.add(Coin("ETH", 43000.0))
+    coinRepo.add(Coin("USDT", 17000.0))
 
 }
