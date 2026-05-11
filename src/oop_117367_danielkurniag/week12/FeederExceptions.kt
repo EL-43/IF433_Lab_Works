a5d4d8230df0c739ecb98a4d5c9375cdc234ddb3
EL-43 <danielkurniagunawan@gmail.com>
2026-05-11 12:31:43 +0700

    week12: (task) create custom feeder exception subclasses

--- src/oop_117367_danielkurniag/week12/FeederExceptions.kt
+++ src/oop_117367_danielkurniag/week12/FeederExceptions.kt
@@ -1,3 +1,7 @@
 package oop_117367_danielkurniag.week12
 
 sealed class FeederException(msg: String) : Exception(msg)
+
+class FoodEmptyException(requested: Int, available: Int) : FeederException("Not enough kibble! requested $requested grams, but only $available grams avilable")
+
+class DispenserJamException: FeederException("Dispenser is jammed!")
