9645d17db364ba7452716b90c898df0ab0223242
EL-43 <danielkurniagunawan@gmail.com>
2026-05-11 12:59:47 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_117367_danielkurniag/week12/SmartFeeder.kt
+++ src/oop_117367_danielkurniag/week12/SmartFeeder.kt
@@ -2,5 +2,8 @@ package oop_117367_danielkurniag.week12
 
 fun dispenseKibble(requestedGrams: Int, availableGrams: Int, isJammed: Boolean): Int {
     require(requestedGrams > 0) { "Requested Grams must be > 0 grams" }
-
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+    return TODO("Provide the return value")
 }
