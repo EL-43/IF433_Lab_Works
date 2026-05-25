2d8296711326e18559364ac661350180201a234f
EL-43 <danielkurniagunawan@gmail.com>
2026-05-25 13:53:34 +0700

    week14: extract EmailService to fix SRP

--- src/oop_117367_danielkurniag/week14/SrpRefactored.kt
+++ src/oop_117367_danielkurniag/week14/SrpRefactored.kt
@@ -9,3 +9,9 @@ class UserRepository {
         println("Saving user: ${user.name} to database.")
     }
 }
+
+class EmailService {
+    fun sendWelcome(user : User){
+        println("Welcome,email -> ${user.email}")
+    }
+}
